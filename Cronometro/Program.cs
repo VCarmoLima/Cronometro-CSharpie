ef6b08160a64f53a3aca02260cb7b54d537afba0
Vinícius <viniciuscarmo.contato@gmail.com>
2023-11-02 18:34:59 -0300

    Implementação de Cronômetro.

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cronometro
@@ -10,6 +12,28 @@ namespace Cronometro
         {
         static void Main(string[] args)
             {
+            CronometroIniciar();
+            }
+
+        static void CronometroIniciar()
+            {
+            Console.Clear();
+            Console.WriteLine("____________________");
+            Console.WriteLine("");
+            Console.Write("Digite quantos segundos quer cronometrar: ");
+            int tempo = int.Parse(Console.ReadLine());
+            int tempoPercorrido = 0;
+
+            while (tempoPercorrido <= tempo)
+                {
+                Console.Clear();
+                Console.WriteLine(tempoPercorrido);
+                Thread.Sleep(999);
+                tempoPercorrido++;
+                }
+            Console.Clear();
+            Console.WriteLine("Cronômetro finalizado!");
+            Thread.Sleep(3000);
             }
         }
     }
