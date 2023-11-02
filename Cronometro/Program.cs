e15f1f076c594a51e0035cca76b5f5d26cc97ecf
Vinícius <viniciuscarmo.contato@gmail.com>
2023-11-02 19:07:42 -0300

    Implementado um Menu com opções do Cronômetro.

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -12,10 +12,44 @@ namespace Cronometro
         {
         static void Main(string[] args)
             {
-            CronometroIniciar();
+            Menu();
             }
 
-        static void CronometroIniciar()
+        static void Menu()
+            {
+            Console.Clear();
+            Console.WriteLine("BEM VINDO AO CRONÔMETRO!");
+            Console.WriteLine("____________________");
+            Console.WriteLine("");
+
+            Console.WriteLine("Você deseja contar em:");
+            Console.WriteLine("1 - Segundos");
+            Console.WriteLine("2 - Minutos");
+            Console.WriteLine("0 - Para sair.");
+
+            Console.Write("Digite sua opção: ");
+            int entradaUsuario = int.Parse(Console.ReadLine());
+
+            switch (entradaUsuario)
+                {
+                case 1:
+                    CronometroSegundos();
+                    break;
+                case 2:
+                    CronometroMinutos();
+                    break;
+                case 0:
+                    System.Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Você digitou uma opção inválida.");
+                    Thread.Sleep(1500);
+                    Menu();
+                    break;
+                }
+            }
+
+        static void CronometroSegundos()
             {
             Console.Clear();
             Console.WriteLine("____________________");
@@ -34,6 +68,30 @@ namespace Cronometro
             Console.Clear();
             Console.WriteLine("Cronômetro finalizado!");
             Thread.Sleep(3000);
+            Menu();
+            }
+
+        static void CronometroMinutos()
+            {
+            Console.Clear();
+            Console.WriteLine("____________________");
+            Console.WriteLine("");
+            Console.Write("Digite quantos minutos quer cronometrar: ");
+            int minuto = int.Parse(Console.ReadLine());
+            int tempo = minuto * 60;
+            int tempoPercorrido = 0;
+
+            while (tempoPercorrido <= tempo)
+                {
+                Console.Clear();
+                Console.WriteLine(tempoPercorrido);
+                Thread.Sleep(999);
+                tempoPercorrido++;
+                }
+            Console.Clear();
+            Console.WriteLine("Cronômetro finalizado!");
+            Thread.Sleep(3000);
+            Menu();
             }
         }
     }
